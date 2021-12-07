c5e8e69515c55d38b4f8046ff6696c2ec6e755e5
Sashanoraa <sasha@sashanoraa.gay>
2021-12-07 15:44:07 -0500

    Make user log in to edit
    Signed-off-by: Sashanoraa <sasha@sashanoraa.gay>

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ from typing import Optional
 
 from fastapi import FastAPI, Form
 from fastapi.exceptions import HTTPException
-from fastapi.param_functions import Depends
+from fastapi.param_functions import Cookie, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm.session import Session
 from starlette.requests import Request
-from starlette.responses import RedirectResponse, Response
+from starlette.responses import RedirectResponse
 
 from app import crud, models, schemas
 
@@ -61,17 +61,23 @@ async def create_post(user: str = Form(...), db: Session = Depends(get_db)):
         return response
 
 @app.get("/edit_page/{page_name}")
-async def edit_page(page_name: str, request: Request, db: Session = Depends(get_db)):
-    page = crud.get_page(db, page_name)
-    if page is None:
-        page = crud.create_page(db, schemas.Page(page_name, ""))
-    return templates.TemplateResponse("edit_page.html", {'request': request, 'name': page.name, 'content': page.content})
+async def edit_page(page_name: str, request: Request, db: Session = Depends(get_db), user: Optional[str] = Cookie(None)):
+    if user:
+        page = crud.get_page(db, page_name)
+        if page is None:
+            page = crud.create_page(db, schemas.Page(page_name, ""))
+        return templates.TemplateResponse("edit_page.html", {'request': request, 'name': page.name, 'content': page.content})
+    else:
+        return RedirectResponse(f"/login", status_code=303)
 
 @app.post("/edit_page")
-async def edit_page_post(name: str = Form(...), content: str = Form(...), db: Session = Depends(get_db)):
-    crud.update_page_content(db, name, content)
-    response = RedirectResponse(f"/page/{name}", status_code=303)
-    return response
+async def edit_page_post(name: str = Form(...), content: str = Form(...), db: Session = Depends(get_db), user: Optional[str] = Cookie(None)):
+    if user:
+        crud.update_page_content(db, name, content)
+        response = RedirectResponse(f"/page/{name}", status_code=303)
+        return response
+    else:
+        return RedirectResponse(f"/login", status_code=303)
 
 @app.get("/page/{page_name}")
 async def page(page_name: str, request: Request, db: Session = Depends(get_db)):
@@ -81,8 +87,11 @@ async def page(page_name: str, request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse("page.html", {'request': request, 'name': page.name, 'content': page.content})
 
 @app.get("/create_page")
-async def create_page(request: Request):
-    return templates.TemplateResponse("create_page.html", {'request': request})
+async def create_page(request: Request, user: Optional[str] = Cookie(None)):
+    if user:
+        return templates.TemplateResponse("create_page.html", {'request': request})
+    else:
+        return RedirectResponse(f"/login", status_code=303)
 
 @app.post("/create_page")
 async def create_page_post(name: str = Form(...)):
